11e5bcb3ad2f2f77b360a1befd9a2475b4ed5b5a
gabrielzezze <gabrielzezze@me.com>
2020-11-09 22:27:51 -0300

    Add args flags for better usage

--- infraestructure/main.py
+++ infraestructure/main.py
@@ -2,20 +2,33 @@ from utilities.aws import init_aws_client
 from utilities.groups.frontend import Frontend
 from utilities.groups.backend import Backend
 import boto3
+import sys
 
-def main():
-    # aws_client = init_aws_client('ec2', 'us-east-1')
-    # ec2_client = boto3.resource('ec2', region_name='us-east-1')
-    # elb_client = init_aws_client('elbv2', 'us-east-1')
-    # frontend = Frontend(aws_client, ec2_client, elb_client)
-    # frontend()
+def handle_frontend_infraestructure():
+    aws_client = init_aws_client('ec2', 'us-east-1')
+    ec2_client = boto3.resource('ec2', region_name='us-east-1')
+    elb_client = init_aws_client('elbv2', 'us-east-1')
 
+    frontend = Frontend(aws_client, ec2_client, elb_client)
+    frontend()
+
+def handle_backend_infraestructrue():
     aws_client = init_aws_client('ec2', 'us-east-2')
     ec2_client = boto3.resource('ec2', region_name='us-east-2')
     backend = Backend(aws_client, ec2_client)
     backend()
-    
-
 
 if __name__ == '__main__':
-    main()
+    args = sys.argv
+
+    if '--frontend' in args:
+        handle_frontend_infraestructure()
+    
+    if '--backend' in args:
+        handle_backend_infraestructrue()
+    
+    if '--frontend' not in args and '--backend' not in args:
+        run_all = str(input('Want to create entire infraestructure?(y/N)'))
+        if run_all == 'y' or run_all == 'y':
+            handle_frontend_infraestructure()
+            handle_backend_infraestructrue()
