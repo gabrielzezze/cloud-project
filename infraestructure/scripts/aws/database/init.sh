99aa61c9fa20d9a5ebcf87abaef7c2a8cbfcf986
gabrielzezze <gabrielzezze@me.com>
2020-11-12 16:25:34 -0300

    Move docker file

--- infraestructure/scripts/aws/database/init.sh
+++ infraestructure/scripts/aws/database/init.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/gabrielzezze/cloud-project.git
 
-sudo docker build -t zezze-custom-mysql ./cloud-project/infraestructure/scripts/docker/
+sudo docker build -t zezze-custom-mysql ./cloud-project/infraestructure/scripts/database/
 sudo docker run --name cloud-mysql-database -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --restart always -p 80:3306 zezze-custom-mysql
