1a9a56a8703b5e7684856cf4fd11d46b77b73c3f
gabrielzezze <gabrielzezze@me.com>
2020-11-12 18:51:05 -0300

    Typo in db script

--- infraestructure/scripts/aws/database.sh
+++ infraestructure/scripts/aws/database.sh
@@ -3,12 +3,12 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install docker
-sudo apt remove docker docker-engine docker.io containerd runc
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt remove docker docker-engine docker.io containerd runc -y
+sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt nstall docker-ce docker-ce-cli containerd.io
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 # Build Custom docker file
 sudo docker build -t zezze-custom-mysql ../database
