55dc9e778c0f007608f8c35d0cf51470f9dcab53
gabrielzezze <gabrielzezze@me.com>
2020-11-13 00:52:38 -0300

    WIP on docker database

--- infraestructure/scripts/aws/database/user_data.sh
+++ infraestructure/scripts/aws/database/user_data.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/gabrielzezze/cloud-project.git
 chmod +x ./cloud-project/infraestructure/scripts/aws/database.sh
 export MYSQL_ROOT_PASSWORD= __password-placeholder__
-./cloud-project/infraestructure/scripts/aws/database/init.sh  
+./cloud-project/infraestructure/scripts/aws/database/init.sh
+sudo docker run --name cloud-mysql-database -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --restart always -p 80:3306 zezze-custom-mysql
