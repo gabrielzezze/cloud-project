6c11926522d472976050663d1875e1144cc147b2
gabrielzezze <gabrielzezze@me.com>
2020-11-13 01:06:36 -0300

    Fix in database scripts

--- infraestructure/scripts/aws/database/init.sh
+++ infraestructure/scripts/aws/database/init.sh
@@ -12,5 +12,4 @@ sudo apt install docker-ce docker-ce-cli containerd.io -y
 
 # Build Custom docker file
 sudo docker build -t zezze-custom-mysql ./cloud-project/infraestructure/scripts/database
-sudo docker run --name cloud-mysql-database -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD --restart always -p 80:3306 zezze-custom-mysql
 
