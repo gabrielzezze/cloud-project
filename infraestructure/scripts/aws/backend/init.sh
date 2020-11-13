e5e9972903ac40e6842859f6c8bdc37686755beb
gabrielzezze <gabrielzezze@me.com>
2020-11-13 01:39:31 -0300

    Fix in backend init.sh

--- infraestructure/scripts/aws/backend/init.sh
+++ infraestructure/scripts/aws/backend/init.sh
@@ -4,13 +4,13 @@ sudo apt upgrade -y
 
 # Install Node + yarn + npm
 # -Node + Npm
-sudo apt install nodejs
-sudo apt install npm
+sudo apt install nodejs -y
+sudo apt install npm -y
 # -Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update
-sudo apt install --no-install-recommends yarn
+sudo apt install --no-install-recommends yarn -y
 
 git clone https://github.com/gabrielzezze/cloud-project-applications.git ./applications
 cd ./applications/backend
