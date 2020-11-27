5cf041e80d26c0f1c2218d3f4efb43a5d6ec9121
gabrielzezze <gabrielzezze@me.com>
2020-11-27 19:17:21 -0300

    Front Adjustements

--- infraestructure/scripts/aws/frontend/init.sh
+++ infraestructure/scripts/aws/frontend/init.sh
@@ -1,4 +1,6 @@
 rm -rf ./applications
 git clone https://github.com/gabrielzezze/cloud-project-applications.git ./applications
 cd ./applications/frontend
+git checkout vpn-version
 yarn install
+yarn run deploy-prod
--- infraestructure/scripts/aws/frontend/user_data.sh
+++ infraestructure/scripts/aws/frontend/user_data.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+# Clone repo
 git clone https://github.com/gabrielzezze/cloud-project.git
+
+# Instal caddy
+echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" \
+    | sudo tee -a /etc/apt/sources.list.d/caddy-fury.list
+sudo apt update
+sudo apt install caddy
+
+cp ./cloud-project/infraestructure/scripts/aws/frontend/Caddyfile ./
+caddy run &
+
+# Run application
 chmod +x ./cloud-project/infraestructure/scripts/aws/frontend/init.sh
 ./cloud-project/infraestructure/scripts/aws/frontend/init.sh
-REACT_APP_BACKEND_IP=$BACKEND_ELASTIC_IP NODE_ENV=production yarn start
