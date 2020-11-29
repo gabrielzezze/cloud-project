1744e5e0d1ee38721f8b075d62d0ae46ec74e2db
gabrielzezze <gabrielzezze@me.com>
2020-11-28 22:44:00 -0300

    Fixing Frontend data forwarding

--- infraestructure/scripts/aws/frontend_outway/user_data.sh
+++ infraestructure/scripts/aws/frontend_outway/user_data.sh
@@ -35,5 +35,5 @@ echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" | sudo tee -a /etc/apt/sou
 sudo apt update
 sudo apt install caddy -y
 
-cp ./cloud-project/infraestructure/scripts/aws/frontend_outway/Caddyfile-template ./Caddyfile
-sudo caddy run
+cp ./cloud-project/infraestructure/scripts/aws/frontend_outway/Caddyfile-template /home/ubuntu/Caddyfile
+# sudo caddy run
